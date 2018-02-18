06f5e9c29326873c6f7b4e9584dcfc8919d63de9
Evan Wang <evanwang0@users.noreply.github.com>
2018-02-17 22:23:17 -0500

    Windows 95 Buttons and URI tweaks

--- www/server.js
+++ www/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
-
 const app = express();
+
 app.disable('x-powered-by');
+app.use(/^\/$/, (req, res, next) => res.redirect(302, "/index.html"));
 app.use(express.static("public"));
 app.listen(8080);
