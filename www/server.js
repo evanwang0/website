45dd6d57911f533896ce8193db9699e3527f6172
Evan Wang <evanwang0@users.noreply.github.com>
2021-04-10 13:41:57 -0400

    Redesign

--- www/server.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const app = express();
-
-app.disable('x-powered-by');
-app.use(/^\/$/, (req, res, next) => res.redirect(302, "/index.html"));
-app.use(express.static("public"));
-app.listen(8080);
